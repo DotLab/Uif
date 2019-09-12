563143d1d69180e99d0b3df007af036cfe66522c
Kailang <kailangfu@gmail.com>
2019-09-11 23:22:16 -0500

    Add GraphicExtension for Graphic.SetAlpha

---
+++ Extensions/GraphicExtension.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Uif.Extensions {
+	public static class GraphicExtension {
+		public static void SetAlpha(this Graphic g, float a) {
+			var c = g.color;
+			c.a = a;
+			g.color = c;
+		}
+	}
+}
+
