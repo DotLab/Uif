d603e48e4c7a7a47f6bedb5ddeed60e380ae7f8d
Kailang <kailangfu@gmail.com>
2019-09-13 12:36:05 -0500

    Add MathExtensions

---
+++ Extensions/MathExtension.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Uif.Extensions {
+	public static class MathExtension {
+		public static Vector2 Vec2(this Vector3 a) {
+			return new Vector2(a.x, a.y);
+		}
+
+		public static Vector2 Div(this Vector2 a, Vector2 b) {
+			return new Vector2(a.x / b.x, a.y / b.y);
+		}
+
+		public static Vector2 Mult(this Vector2 a, Vector2 b) {
+			return new Vector2(a.x * b.x, a.y * b.y);
+		}
+	}
+}
+
