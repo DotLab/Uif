c9c4272d10ad3a2628a19f5df4464020585fba85
Kailang <kailangfu@gmail.com>
2019-09-21 16:00:29 -0500

    Add Binding

---
+++ Binding/MemberBinding.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using UnityEngine;
+using Jsonf;
+
+namespace Uif.Binding {
+	public sealed class MemberBinding : MonoBehaviour {
+		public GameObject target;
+		public string targetMemberName;
+
+		[Space]
+		public MonoBehaviour targetComponent;
+		public string targetTypeName;
+		ReflectionContext.Member targetMember;
+
+		[Header("Two-Way Binding")]
+		public bool isTwoWay;
+		public string selfMemberName;
+
+		[Space]
+		public MonoBehaviour selfComponent;
+		public string selfTypeName;
+		ReflectionContext.Member selfMember;
+
+		void OnValidate() {
+			targetComponent = null;
+			targetTypeName = null;
+			selfComponent = null;
+			selfTypeName = null;
+
+			var refCtx = new ReflectionContext();
+
+			foreach (var c in target.GetComponents<MonoBehaviour>()) {
+				var member = FindMember(refCtx, c, targetMemberName);
+				if (member != null) {
+					targetComponent = c;
+					targetTypeName = member.type.FullName;
+					targetMember = member;
+				}
+			}
+
+			if (isTwoWay) {
+				foreach (var c in GetComponents<MonoBehaviour>()) {
+					var member = FindMember(refCtx, c, selfMemberName);
+					if (member != null) {
+						selfComponent = c;
+						selfTypeName = member.type.FullName;
+						selfMember = member;
+					}
+				}
+			}
+		}
+
+		void Start() {
+			var refCtx = new ReflectionContext();
+
+			targetMember = FindMember(refCtx, targetComponent, targetMemberName);
+
+			if (isTwoWay) {
+				selfMember = FindMember(refCtx, selfComponent, selfMemberName);
+				SetSelfValue(targetMember.Get(targetComponent));
+				if (targetComponent is TwoWayBindable) {
+					((TwoWayBindable)targetComponent).AddBinding(targetMemberName, this);
+				}
+			}
+		}
+
+		public void SetTargetValue(object value) {
+			targetMember.Set(targetComponent, Convert.ChangeType(value, targetMember.type));
+		}
+
+		public void SetSelfValue(object value) {
+			selfMember.Set(selfComponent, Convert.ChangeType(value, selfMember.type));
+		}
+
+		static ReflectionContext.Member FindMember(ReflectionContext refCtx, object component, string name) {
+			var type = component.GetType();
+
+			foreach (var m in refCtx.GetMembers(type)) {
+				if (m.name == name) {
+					return m;
+				}
+			}
+
+			return null;
+		}
+	}
+}
+
---
+++ Binding/TwoWayBindable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Uif.Binding {
+	public class TwoWayBindable : MonoBehaviour {
+		readonly Dictionary<string, MemberBinding> bindingDict = new Dictionary<string, MemberBinding>();
+
+		public void AddBinding(string name, MemberBinding binding) {
+			bindingDict[name] = binding;
+		}
+
+		public void BackPropagateValue(string name, object value) {
+			MemberBinding binding;
+			if (bindingDict.TryGetValue(name, out binding)) {
+				binding.SetSelfValue(value);
+			}
+		}
+	}
+}
+
